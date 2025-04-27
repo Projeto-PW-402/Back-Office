c9c9b1d85df728506b2e510f01ff624f6f0f5f9f
notmyfault <fallenangelhd06@gmail.com>
2025-04-27 16:13:40 +0100

    Compatibilidade com varias resolucoes

---
+++ src/subpages/NewUser.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="main-container">
+        <form>
+            <label for="name">Nome</label>
+            <input type="text" placeholder="" />
+            <label for="name">Morada</label>
+            <input type="text" placeholder="" />
+            <label for="name">Email</label>
+            <input type="email" placeholder="" />
+        </form>
+    </div>
+</template>
+<style scoped>
+.main-container {
+    position: absolute;
+    top: calc(50% - 250px);
+    left: calc(50% - 50px);
+    width: 500px;
+    height: 500px;
+    background-color: red;
+}
+</style>
