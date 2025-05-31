fa2d36f7106fa47edc3cd7d05c0eaf552aca43b3
notmyfault <fallenangelhd06@gmail.com>
2025-05-31 21:55:47 +0100

    add dist

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-import Dashboard from './pages/Dashboard.vue';
-import Navbar from './components/Navbar.vue';
-</script>
-<template>
-    <RouterView/>
-</template>
