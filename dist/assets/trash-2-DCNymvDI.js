e1ebea87e31c8b77744d20649f077074a23cef71
notmyfault <fallenangelhd06@gmail.com>
2025-06-01 00:10:02 +0100

    add dist

--- dist/assets/trash-2-DCNymvDI.js
+++ dist/assets/trash-2-DCNymvDI.js
@@ -1,4 +1,4 @@
-import{c as e}from"./Navbar-Dj42rLpa.js";/**
+import{c as e}from"./Navbar-DxcdHLe8.js";/**
  * @license lucide-vue-next v0.485.0 - ISC
  *
  * This source code is licensed under the ISC license.
